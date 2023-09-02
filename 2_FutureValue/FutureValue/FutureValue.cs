4d6844cd30f6525b033be4bab25fb2bd719b8135
lchz <lauraz940829@hotmail.com>
2023-09-02 12:22:34 +0300

    Functioned Input data validity checking

--- 2_FutureValue/FutureValue/FutureValue.cs
+++ 2_FutureValue/FutureValue/FutureValue.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -29,9 +30,33 @@ namespace FutureValue
         /// <param name="e"></param>
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            double monthlyInvestment = Convert.ToDouble(txtMonInvestment.Text);
-            double yearlyInterestRate = Convert.ToDouble(txtYearlyInterestRate.Text);
-            int years = Convert.ToInt32(txtNumYears.Text);
+            double monthlyInvestment;
+            double yearlyInterestRate;
+            int years;
+
+            // Check inputs validity
+            if (!(IsValidData(txtMonInvestment, "Monthly Investment", 10, 2000)
+                && IsValidData(txtYearlyInterestRate, "Yearly Interest Rate", 1, 200)
+                && IsValidData(txtNumYears, "Number of Years", 1, 20, false)))
+            {
+                return;
+            }
+            
+
+            // Check any other possible excdeptions
+            try
+            {
+                monthlyInvestment = Convert.ToDouble(txtMonInvestment.Text);
+                yearlyInterestRate = Convert.ToDouble(txtYearlyInterestRate.Text);
+                years = Convert.ToInt32(txtNumYears.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception Error");
+                txtMonInvestment.Focus();
+                return;
+            }
+
 
             double monInterestRate = yearlyInterestRate / 12 / 100;
             double futureValue = CalculateFutureValue(monthlyInvestment, years, monInterestRate);
@@ -81,5 +106,103 @@ namespace FutureValue
         {
             txtYearlyInterestRate.Text = Convert.ToString(12);
         }
+
+        /// <summary>
+        /// Validate input data
+        /// </summary>
+        /// <returns></returns>
+        private bool IsValidData(TextBox txtbox, string txtName, double minValue, double maxValue, bool isDoubleValue=true)
+        {
+            if (isDoubleValue)
+            {
+                if (IsPresent(txtbox, txtName)
+                    && IsDouble(txtbox, txtName)
+                    && IsWithinRange(txtbox, txtName, minValue, maxValue))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (IsPresent(txtbox, txtName)
+                    && IsInt32(txtbox, txtName)
+                    && IsWithinRange(txtbox, txtName, minValue, maxValue))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// Check empty input
+        /// </summary>
+        /// <param name="txtBox"></param>
+        /// <param name="txtName"></param>
+        /// <returns></returns>
+        private bool IsPresent(TextBox txtBox, string txtName)
+        {
+            
+            if (txtBox.Text == "")
+            {
+                MessageBox.Show(txtName + " is a required field.", "Entry Error");
+                txtBox.Focus();
+                return false;
+            }
+
+            return true;
+        }
+        /// <summary>
+        /// Check if input format is Double
+        /// </summary>
+        /// <param name="txtBox"></param>
+        /// <param name="txtName"></param>
+        /// <returns></returns>
+        private bool IsDouble(TextBox txtBox, string txtName)
+        {
+            if (!Double.TryParse(txtBox.Text, out double d))
+            {
+                MessageBox.Show(txtName + " requires a decimal value.", "Format Error");
+                txtBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        /// <summary>
+        /// Check if input format is integer
+        /// </summary>
+        /// <param name="txtBox"></param>
+        /// <param name="txtName"></param>
+        /// <returns></returns>
+        private bool IsInt32(TextBox txtBox, string txtName)
+        {
+            if (!Int32.TryParse(txtBox.Text, out int i))
+            {
+                MessageBox.Show(txtName + " requires an integer value.", "Format Error");
+                txtBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        /// <summary>
+        /// Check if input within the range
+        /// </summary>
+        /// <param name="txtBox"></param>
+        /// <param name="txtName"></param>
+        /// <param name="minValue"></param>
+        /// <param name="MaxValue"></param>
+        /// <returns></returns>
+        private bool IsWithinRange(TextBox txtBox, string txtName, double minValue, double MaxValue)
+        {
+            double inputValue = Convert.ToDouble(txtBox.Text);
+            if (minValue > inputValue || MaxValue < inputValue)
+            {
+                MessageBox.Show(txtName + " is not in a valid range.", "Range Error");
+                txtBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
     }
 }
