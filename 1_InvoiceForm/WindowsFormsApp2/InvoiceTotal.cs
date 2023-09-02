82ce5fe7d896b4d89ca52747afcc350ace8864f3
lchz <lauraz940829@hotmail.com>
2023-09-02 17:13:06 +0300

    Array and List practice.

--- 1_InvoiceForm/WindowsFormsApp2/InvoiceTotal.cs
+++ 1_InvoiceForm/WindowsFormsApp2/InvoiceTotal.cs
@@ -17,22 +17,92 @@ namespace WindowsFormsApp2
             InitializeComponent();
         }
 
-        private void btnExit_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
 
         int numInvoices = 0;
         double totalInvoices = 0;
         double invoiceAve = 0;
         double largestInvoice = 0;
         double smallestInvoice = Double.MaxValue;
+
+        double[] invoicesArray = new double[5];
+        int index = 0;
+
+        List<double> invoicesList = new List<double>();
+
         /// <summary>
         /// Calculate-btn functions
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (!IsSubtotalInputValid())
+            {
+                return;
+            }
+
+            double subtotal = Convert.ToDouble(txtEnterSubtotal.Text);
+            string customerType = txtCustomerType.Text;
+            double disPercent = GetDiscountPercent(subtotal, customerType);
+
+            double disAmount = subtotal * disPercent;
+            double total = subtotal - disAmount;
+
+            txtSubtotal.Text = subtotal.ToString("c");
+            txtDisPercent.Text = disPercent.ToString("p1");
+            txtDisAmount.Text = disAmount.ToString("c");
+            txtTotal.Text = total.ToString("c");
+
+            UpdateInvoiceSummary(total);
+            txtEnterSubtotal.Focus();
+
+            // Store up to 5 invoices
+            if (index < invoicesArray.Length)
+            {
+                invoicesArray[index] = total;
+                index++;
+            }
+
+            // Store invoices in List
+            if (invoicesList.Count < 5)
+            {
+                invoicesList.Add(total);
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Show totals in array when closing the app.
+            Array.Sort(invoicesArray); // Sort
+            string allTotals = "";
+            foreach (double total in invoicesArray)
+            {
+                if (total != 0)
+                {
+                    allTotals += total.ToString("c") + "\n";
+                }
+                
+            }
+            MessageBox.Show(allTotals, "Order Totals in Array");
+
+            // Show totals in list
+            invoicesList.Sort();
+            allTotals = "";
+            foreach (double total in invoicesArray)
+            {
+                if (total != 0)
+                {
+                    allTotals += total.ToString("c") + "\n";
+                }
+
+            }
+            MessageBox.Show(allTotals, "Order Totals in List");
+
+            this.Close();
+        }
+
+        private bool IsSubtotalInputValid()
         {
             // Check if Subtotal is empty
             if (txtEnterSubtotal.Text == "")
@@ -41,23 +111,17 @@ namespace WindowsFormsApp2
                     "Subtotal is required.",
                     "Entry Error");
                 txtEnterSubtotal.Focus();
-                return;
+                return false;
             }
-
-            // Subtotal format check
-            double subtotal = 0;
-            try
-            {
-                subtotal = Convert.ToDouble(txtEnterSubtotal.Text);
-            }
-            catch (FormatException fe)
+            // Check if Subtotal a number
+            if (!Double.TryParse(txtEnterSubtotal.Text, out double subtotal))
             {
                 MessageBox.Show(
                     "Please enter a valid number for the Subtotal field.",
                     "Entry Error");
 
                 txtEnterSubtotal.Focus();
-                return; // Not execute the rest if subtotal is not assigned a value.
+                return false;
             }
 
             // Subtotal range check
@@ -67,12 +131,45 @@ namespace WindowsFormsApp2
                     "The subtotal should greater than 0 and less than 10,000.",
                     "Entry Error");
                 txtEnterSubtotal.Focus();
-                return;
+                return false;
             }
 
-            string customerType = txtCustomerType.Text;
-            double disPercent = 0.1;
+            return true;
+        }
 
+        /// <summary>
+        /// Clear the count of the invoices.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClearTotals_Click(object sender, EventArgs e)
+        {
+            numInvoices = 0;
+            totalInvoices = 0;
+            invoiceAve = 0;
+
+            txtNumInvoices.Text = "";
+            txtTotalInvoices.Text = "";
+            txtInvoiceAve.Text = "";
+
+            txtEnterSubtotal.Focus();
+
+        }
+
+        /*
+         * ************ HELP FUNCTIONS **********************
+         */
+
+        /// <summary>
+        /// Set different discount percents for different customer types.
+        /// Initial discount percent is 0.1.
+        /// </summary>
+        /// <param name="subtotal"></param>
+        /// <param name="customerType"></param>
+        /// <param name="disPercent">Initial discount percent. Always 0.1</param>
+        /// <returns></returns>
+        private static double GetDiscountPercent(double subtotal, string customerType, double disPercent=0.1)
+        {
             if (customerType == "R")
             {
                 if (subtotal >= 250 && subtotal < 500)
@@ -100,6 +197,8 @@ namespace WindowsFormsApp2
                 }
             }
 
+            return disPercent;
+
             //// Example use of switch
             //switch (customerType)
             //{
@@ -113,15 +212,14 @@ namespace WindowsFormsApp2
             //        disPercent = 0.1;
             //        break;
             //}
+        }
 
-            double disAmount = subtotal * disPercent;
-            double total = subtotal - disAmount;
-
-            txtSubtotal.Text = subtotal.ToString("c");
-            txtDisPercent.Text = disPercent.ToString("p1");
-            txtDisAmount.Text = disAmount.ToString("c");
-            txtTotal.Text = total.ToString("c");
-
+        /// <summary>
+        /// Update invoice summary: count, total, ave, largest and smallest invoice.
+        /// </summary>
+        /// <param name="total"></param>
+        private void UpdateInvoiceSummary(double total)
+        {
             // Update number, total and ave of invoices
             numInvoices++;
             totalInvoices += total;
@@ -130,34 +228,14 @@ namespace WindowsFormsApp2
             txtNumInvoices.Text = numInvoices.ToString();
             txtTotalInvoices.Text = totalInvoices.ToString("c");
             txtInvoiceAve.Text = invoiceAve.ToString("c");
-
             // Update largest, smallest invoice
             largestInvoice = Math.Max(largestInvoice, total);
             smallestInvoice = Math.Min(smallestInvoice, total);
             txtLargestInvoice.Text = largestInvoice.ToString("c");
             txtSmallestInvoice.Text = smallestInvoice.ToString("c");
-
-            txtEnterSubtotal.Focus();
         }
 
 
-        /// <summary>
-        /// Clear the count of the invoices.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void btnClearTotals_Click(object sender, EventArgs e)
-        {
-            numInvoices = 0;
-            totalInvoices = 0;
-            invoiceAve = 0;
-
-            txtNumInvoices.Text = "";
-            txtTotalInvoices.Text = "";
-            txtInvoiceAve.Text = "";
-
-            txtEnterSubtotal.Focus();
-
-        }
+        
     }
 }
