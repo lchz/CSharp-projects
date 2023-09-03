9cc6f18236d624ea74b1cadc365dd343f82a6a06
lchz <lauraz940829@hotmail.com>
2023-09-03 13:08:22 +0300

    Work sith strings - Parse name

---
+++ 4_StringHandling/4_StringHandling/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace _4_StringHandling
+{
+    partial class StringHandlingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labName = new System.Windows.Forms.Label();
+            this.btnParseName = new System.Windows.Forms.Button();
+            this.btnEditPhoneNumber = new System.Windows.Forms.Button();
+            this.labPhoneNumber = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labName
+            // 
+            this.labName.AutoSize = true;
+            this.labName.Location = new System.Drawing.Point(13, 38);
+            this.labName.Name = "labName";
+            this.labName.Size = new System.Drawing.Size(55, 20);
+            this.labName.TabIndex = 0;
+            this.labName.Text = "Name:";
+            // 
+            // btnParseName
+            // 
+            this.btnParseName.Location = new System.Drawing.Point(149, 162);
+            this.btnParseName.Name = "btnParseName";
+            this.btnParseName.Size = new System.Drawing.Size(108, 31);
+            this.btnParseName.TabIndex = 3;
+            this.btnParseName.Text = "Parse Name";
+            this.btnParseName.UseVisualStyleBackColor = true;
+            this.btnParseName.Click += new System.EventHandler(this.btnParseName_Click);
+            // 
+            // btnEditPhoneNumber
+            // 
+            this.btnEditPhoneNumber.Location = new System.Drawing.Point(123, 214);
+            this.btnEditPhoneNumber.Name = "btnEditPhoneNumber";
+            this.btnEditPhoneNumber.Size = new System.Drawing.Size(172, 31);
+            this.btnEditPhoneNumber.TabIndex = 4;
+            this.btnEditPhoneNumber.Text = "Edit Phone Number";
+            this.btnEditPhoneNumber.UseVisualStyleBackColor = true;
+            // 
+            // labPhoneNumber
+            // 
+            this.labPhoneNumber.AutoSize = true;
+            this.labPhoneNumber.Location = new System.Drawing.Point(13, 96);
+            this.labPhoneNumber.Name = "labPhoneNumber";
+            this.labPhoneNumber.Size = new System.Drawing.Size(119, 20);
+            this.labPhoneNumber.TabIndex = 5;
+            this.labPhoneNumber.Text = "Phone Number:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(166, 37);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(166, 96);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(100, 26);
+            this.txtPhoneNumber.TabIndex = 9;
+            // 
+            // StringHandlingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 301);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.labPhoneNumber);
+            this.Controls.Add(this.btnEditPhoneNumber);
+            this.Controls.Add(this.btnParseName);
+            this.Controls.Add(this.labName);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "StringHandlingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PhoneBook";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labName;
+        private System.Windows.Forms.Button btnParseName;
+        private System.Windows.Forms.Button btnEditPhoneNumber;
+        private System.Windows.Forms.Label labPhoneNumber;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+    }
+}
+
---
+++ 4_StringHandling/4_StringHandling/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_StringHandling
+{
+    public partial class StringHandlingForm : Form
+    {
+        public StringHandlingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParseName_Click(object sender, EventArgs e)
+        {
+            string[] name = txtName.Text.Trim().Split(' ');
+            
+            if (name.Length < 2)
+            {
+                MessageBox.Show("Please enter at least first name and last name.", "Name Format Error");
+                txtName.Focus();
+                return;
+            }
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                name[i] = name[i].Replace(name[i][0], char.ToUpper(name[i][0]));
+                for (int j = 1; j < name[i].Length; j++)
+                {
+                    name[i] = name[i].Replace(name[i][j], char.ToLower(name[i][j]));
+                }
+            }
+
+
+            if (name.Length == 2)
+            {
+                MessageBox.Show(
+                    $"{"First name:",-15}{name[0],-5}\n" +
+                    $"{"Last name:",-14}{name[1],-5}",
+                    "Parse Name");
+            }
+            else if (name.Length == 3)
+            {
+                MessageBox.Show(
+                    $"{"First name:",-20}{name[0],-5}\n" +
+                    $"{"Middle name:",-16}{name[1],-5}\n" +
+                    $"{"Last name:",-19}{name[2],-5}",
+                    "Parse Name");
+            }
+        }
+    }
+}
